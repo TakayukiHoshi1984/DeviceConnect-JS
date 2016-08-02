3a89585ac69579522557751b1c1a17fb164dd2bf
Masaru Takano <mtaka@gclue.jp>
2016-08-02 20:45:41 +0900

    HostデバイスプラグインのServiceInformationAPIの単体テストを追加。

--- dConnectSDKForJavascriptTest/tests/profiles/serviceinformation_normal.js
+++ dConnectSDKForJavascriptTest/tests/profiles/serviceinformation_normal.js
@@ -23,6 +23,10 @@ var ServiceInformationProfileNormalTest = {};
  * </p>
  */
 ServiceInformationProfileNormalTest.serviceInformationTest001 = function(assert) {
+  if (EXPECTED_APIS === undefined) {
+    assert.ok(false, 'Test bug: EXPECTED_APIS is undefined.');
+    QUnit.start();
+  }
   searchTestService(function(accessToken, serviceId) {
     var builder = new dConnect.URIBuilder();
     builder.setProfile(dConnect.constants.serviceinformation.PROFILE_NAME);
@@ -32,8 +36,20 @@ ServiceInformationProfileNormalTest.serviceInformationTest001 = function(assert)
     dConnect.get(uri, null, function(json) {
       assert.ok(true, 'result=' + json.result);
       assert.ok(true, 'version=' + json.version);
-      assert.ok(json.connect !== undefined, 'connect is ok.');
-      assert.ok(json.supports !== undefined, 'supports is ok.');
+      assert.ok(json.connect !== undefined, 'connect is defined.');
+      assert.ok(json.supports !== undefined, 'supports is defined.');
+      assert.ok(json.supportApis !== undefined, 'supportApis is defined.');
+
+      // API仕様を過不足なく取得できていること
+      for (var name in EXPECTED_APIS) {
+        var spec = json.supportApis[name];
+        for (var path in EXPECTED_APIS[name]) {
+          for (var methodIndex in EXPECTED_APIS[name][path]) {
+            var method = EXPECTED_APIS[name][path][methodIndex];
+            assert.ok(spec.paths[path] !== undefined, method.toUpperCase() + ' /gotapi/' + name + ((path === '/') ? '' : path));
+          }
+        }
+      }
       QUnit.start();
     }, function(errorCode, errorMessage) {
       assert.ok(checkErrorCode(errorCode), 'errorCode=' + errorCode + ' errorMessage=' + errorMessage);
